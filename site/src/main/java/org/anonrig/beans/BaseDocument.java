2db62fc6f0e0945ec540c37d7404f858837aeb14
Yagiz Nizipli <yagiznizipli@me.com>
2018-07-09 13:34:19 +0300

    :sparkles: Initial commit

---
+++ site/src/main/java/org/anonrig/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.anonrig.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="anonrig:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
